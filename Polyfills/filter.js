6efe63507fcfcd75521fc7e56428bd3dcf65ba07
aasritha917 <aasrithaaishu@gmail.com>
2025-08-29 17:05:05 +0530

    Assessemnt

---
+++ Polyfills/filter.js
@@ -0,0 +1,9 @@
+Array.prototype.myFilter = function(callback) {
+  let result = [];
+  for (let i = 0; i < this.length; i++) {
+    if (callback(this[i], i, this)) {
+      result.push(this[i]);
+    }
+  }
+  return result;
+};
